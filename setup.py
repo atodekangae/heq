a8ee985d42bb5f5ce7b4d7f017cb819ad2b9ecb9
atodekangae <atodekangae@gmail.com>
2024-01-06 10:14:59 +0900

    Add a Github Action for testing

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / 'README.md').read_text()
+
+setup(
+    name='heq',
+    version='0.0.1',
+    description="Yet another 'jq for HTML'",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='atodekangae',
+    author_email='atodekangae@gmail.com',
+    py_modules=['heq'],
+    python_requires='>=3.8',
+    install_requires=[
+        'lxml>=4',
+        'parsimonious>=0.10.0',
+    ],
+    extras_require={
+        'dev': ['pytest>=6.0,<7.0'],
+    },
+    entry_points={
+        'console_scripts': [
+            'heq=heq:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ]
+)
+
